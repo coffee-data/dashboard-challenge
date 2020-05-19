571785555556fca338dc6ee9feb19cf154e07d5d
coffee-data <jesse.ryan.hartman@gmail.com>
2020-05-19 13:23:54 -0500

    Assignment fully functional.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,6 +5,26 @@ function unpack(rows, index) {
   });
 }
 
+// Function for updating metadata
+function update(data) {
+
+  demoInfo = d3.select("#sample-metadata");
+
+  demoInfo.html("");
+
+  var selection = d3.select("#sample-metadata").selectAll("ul")
+      .data(data);
+
+  selection.enter()
+      .append("li")
+      .merge(selection)
+      .text(function(d) {
+      return d
+      });
+
+selection.exit().remove();
+}
+
 // This filter works!
 function filteredIds(obj) {
   return parseInt(obj.id) == idSelect;
@@ -23,11 +43,6 @@ dropdown.on("change", function() {
   console.log(idSelect);
 });
 
-// // Try this for selection
-// function filteredIds(obj, sel) {
-//   return parseInt(obj.id) === sel;
-// };
-
 // Create async function for reading json
 (async function(){
   var data = await d3.json("./data/samples.json").catch(function(error) {
@@ -35,7 +50,10 @@ dropdown.on("change", function() {
   });
 
 // Log data
+console.log(`Somehow logging...`);
 console.log(data);
+metadata = data.metadata;
+console.log(data.metadata);
 
 // Assign the data names to variable for the select element
 names = data.names;
@@ -57,8 +75,9 @@ options.text(function(d) {
 sampleData = data.samples;
 console.log(sampleData);
 
-// Test sorting
+// Filtering objects based on init
 filteredSample = sampleData.filter(filteredIds);
+filteredMetaData = metadata.filter(filteredIds);
 
 // This sorting works!
 sortedSample = filteredSample.sort((a, b) => (b.sample_values - a.sample_values));
@@ -68,7 +87,21 @@ slicedIds = sortedSample[0].otu_ids.slice(0,10);
 slicedLabels = sortedSample[0].otu_labels.slice(0,10);
 
 function init() {
-// Graph the default selection
+// Initial metadata
+var filteredMetaData = metadata.filter(filteredIds);
+
+// Assign empty arr for readOut
+var readOut = []
+
+// Create string elementd for new array
+Object.entries(filteredMetaData[0]).forEach(([key, value]) => {
+    readOut.push(`${key}: ${value}`);
+   });
+
+// Update the metadata list
+update(readOut);
+
+// The initial bar chart
 var trace1 = {
   x: slicedSampleValues,
   y: slicedIds.map(d => `OTU ${d}`),
@@ -91,8 +124,34 @@ var layout = {
   }
 };
 
-Plotly.newPlot("plot", plotData, layout);
+Plotly.newPlot("bar", plotData, layout);
+
+// The initial bubble chart
+var bubbleTrace = {
+  x: slicedIds,
+  y: slicedSampleValues,
+  text: slicedLabels,
+  mode: 'markers',
+marker: {
+    color: slicedIds,
+    size: slicedSampleValues
+  }
+};
+
+var bubbleData = [bubbleTrace];
+
+var layout = {
+  title: `Name: ${idSelect}`,
+  showlegend: false,
+  yaxis: {
+    automargin: true
+  }
+};
+
+Plotly.newPlot('bubble', bubbleData, layout);
 }
+
+
 
 // On change to the DOM, call optionChanged()
 d3.selectAll("#selDataset").on("change", optionChanged);
@@ -114,6 +173,21 @@ function optionChanged() {
   slicedIds = sortedSample[0].otu_ids.slice(0,10);
   slicedLabels = sortedSample[0].otu_labels.slice(0,10);
 
+  // Event change for metadata
+  var filteredMetaData = metadata.filter(filteredIds);
+
+  // Assign empty arr for readOut
+  var readOut = []
+
+  // Create string elementd for new array
+  Object.entries(filteredMetaData[0]).forEach(([key, value]) => {
+      readOut.push(`${key}: ${value}`);
+    });
+
+  // Update the metadata list
+  update(readOut);
+
+  // Event change data for bar graph
   var trace = {
         x: slicedSampleValues,
         y: slicedIds.map(d => `OTU ${d}`),
@@ -137,8 +211,31 @@ function optionChanged() {
   };
 
   console.log(plotData);
-  Plotly.newPlot("plot", plotData, layout);
+  Plotly.newPlot("bar", plotData, layout);
   
+  // Event change data for bubble chart
+  var bubbleTrace = {
+    x: slicedIds,
+    y: slicedSampleValues,
+    text: slicedLabels,
+    mode: 'markers',
+  marker: {
+      color: slicedIds,
+      size: slicedSampleValues
+    }
+  };
+
+  var bubbleData = [bubbleTrace];
+
+  var layout = {
+    title: `Name: ${idSelect}`,
+    showlegend: false,
+    yaxis: {
+      automargin: true
+    },
+  };
+
+  Plotly.newPlot('bubble', bubbleData, layout);
 }
 
 init();
