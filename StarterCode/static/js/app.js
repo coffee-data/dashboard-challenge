f46d59c69fd7a9965a5421be6dc0c2fdcc9ec624
coffee-data <jesse.ryan.hartman@gmail.com>
2020-05-18 19:46:59 -0500

    Added appending for select element

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,20 +1,150 @@
-// Read in json data using async
+// Create unpack function
+function unpack(rows, index) {
+  return rows.map(function(row) {
+    return row[index];
+  });
+}
+
+// This filter works!
+function filteredIds(obj) {
+  return parseInt(obj.id) === 940;
+};
+
+// // Try this for selection
+// function filteredIds(obj, sel) {
+//   return parseInt(obj.id) === sel;
+// };
+
+// Create async function for reading json
 (async function(){
-    var data = await d3.json("./data/samples.json").catch(function(error) {
-      console.log(error);
-    });
-    console.log(data);
-  
-    // log a list of names
-    var names = data.map(function(x) {
-        return x.name;
-    });
-    console.log("names", names);
-  
-    // Cast each hours value in tvData as a number using the unary + operator
-    tvData.forEach(function(d) {
-      d.hours = +d.hours;
-      console.log("Name:", d.name);
-      console.log("Hours:", d.hours);
-    });
-  })()
+  var data = await d3.json("./data/samples.json").catch(function(error) {
+    console.log(error);
+  });
+
+// Log data
+console.log(data);
+
+// Assign the data names to variable for the select element
+names = data.names;
+
+// Append select element with name data
+var options = d3.select("select")
+  .selectAll("option")
+  .data(names)
+  .enter()
+  .append("option");
+
+options.text(function(d) {
+  return d
+  })
+      .attr("value", function(d) {
+  return d;
+  });
+
+sampleData = data.samples;
+console.log(sampleData);
+
+// Test sorting
+filteredSample = sampleData.filter(filteredIds);
+
+// This sorting works!
+sortedSample = filteredSample.sort((a, b) => (b.sample_values - a.sample_values));
+
+slicedSampleValues = sortedSample[0].sample_values.slice(0,10);
+slicedIds = sortedSample[0].otu_ids.slice(0,10);
+slicedLabels = sortedSample[0].otu_labels.slice(0,10);
+
+
+
+})()
+
+// sampleData.forEach(function(d) {
+//   console.log(d.sample_values);
+//   console.log(d.otu_ids);
+//   console.log(d.otu_labels);
+// })
+
+// Map Sample Values
+// sampleValues = sampleData.map(function(d) {
+//   return d.sample_values;
+// });
+
+// // Map OTU Ids
+// sampleIds = sampleData.map(function(d) {
+//   return d.otu_ids;
+// });
+
+// // Map OTU Labels
+// sampleLabels = sampleData.map(function(d) {
+//   return d.otu_labels;
+// });
+
+
+
+
+
+
+
+
+
+// // Create async function for reading json
+// (async function(){
+//     var bellyData = await d3.json("./data/samples.json").catch(function(error) {
+//       console.log(error);
+//     });
+//     console.log(bellyData);
+   
+//     sampleValues = bellyData.samples.map(function(d) {
+//       return d.sample_values;
+//     });
+//     console.log(sampleValues);
+
+//     labels = bellyData.samples.map(function(d) {
+//       return d.otu_ids;
+//     });
+
+//     hoverText = bellyData.samples.map(function(d) {
+//       return d.otu_labels;
+//     });
+
+//     var trace1 = {
+//       x: labels,
+//       y: sampleValues,
+//       type: "bar"
+//     };
+
+//     var barData = [trace1];
+
+//     var layout = {
+//       title: "'Bar' Chart"
+//     };
+
+//     Plotly.newPlot("plot", barData, layout);
+
+//   })()
+
+
+
+
+  // var metadata = bellyData.metadata;
+  // console.log(`Metadata`);
+  // console.log(bellyData.metadata);
+
+  // var names = bellyData.names;
+  // console.log(`Data Names`);
+  // console.log(bellyData.names);
+
+  // var sampleLook = bellyData.sample;
+  // console.log(`Samples`);
+  // console.log(bellyData.sample);
+
+  // // var sampleValues = bellyData.samples.forEach(function(data) {
+  // //   console.log(data.sample_values);
+  // // })
+
+  // var sampleValues = bellyData.samples.map(d => d.sample_values);
+  // console.log(sampleValues);
+
+  // // var sampleIds = bellyData.samples.forEach(function(data) {
+  // //   console.log(data.otu_ids);
+  // // })
